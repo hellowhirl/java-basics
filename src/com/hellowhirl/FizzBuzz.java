70131f85d14c37ad7362470033176e54b78b414e
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-10 09:21:14 +0900

    loops: for, while, do while

---
+++ src/com/hellowhirl/FizzBuzz.java
@@ -0,0 +1,27 @@
+package com.hellowhirl;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        String message = "";
+
+        System.out.print("Number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int userNumberInput = scanner.nextInt();
+
+        // should have the most specific conditions on the top, and most generic on the bottom
+        if ((userNumberInput % 3 == 0) && (userNumberInput % 5 == 0))
+            message = "fizz buzz";
+        else if (userNumberInput % 3 == 0)
+            message = "fizz";
+        else if (userNumberInput % 5 == 0)
+            message = "buzz";
+        else
+            message = String.valueOf(userNumberInput);
+
+        System.out.println(message);
+    }
+}
--- src/com/hellowhirl/Main.java
+++ src/com/hellowhirl/Main.java
@@ -7,24 +7,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "";
+        //loops - when we know ahead of time how many time we want to execute statements
 
-        System.out.print("Number: ");
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World " + i);
 
-        Scanner scanner = new Scanner(System.in);
-        int userNumberInput = scanner.nextInt();
+        for (int j = 5; j >= 1; j--)
+            System.out.println("Hello World " + j);
 
-        // should have the most specific conditions on the top, and most generic on the bottom
-        if ((userNumberInput % 3 == 0) && (userNumberInput % 5 == 0))
-            message = "fizz buzz";
-        else if (userNumberInput % 3 == 0)
-            message = "fizz";
-        else if (userNumberInput % 5 == 0)
-            message = "buzz";
-        else
-            message = String.valueOf(userNumberInput);
+        // while loops - better when we don't know how many times we want to repeat something
 
-        System.out.println(message);
+        Scanner scanner = new Scanner(System.in); // better to put Scanner object outside of while loop (only need one)
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase(); // ensures that command executes regardless of case entered by user
+            System.out.println(input); // echo back what user enters
+        }
+
+        // do while loop - gets executed at least once (use case is rare)
+
+        do {
+            // code gets executed at least once
+            System.out.println("Do while loop");
+        } while (input == "will only continue if while loop evalutes to true");
     }
 }
 
