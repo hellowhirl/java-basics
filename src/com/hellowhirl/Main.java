5ea4972897e775766a44e35b2a63f03e1381b989
Anthony Avila <anthony.p.avila@gmail.com>
2021-03-11 21:11:18 +0900

    refactor MOrtgage calculator, simplify break statements

--- src/com/hellowhirl/Main.java
+++ src/com/hellowhirl/Main.java
@@ -9,43 +9,45 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0; // better to initiate with some value
+        float monthly = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
-        int principal;
+
         while (true) {
             System.out.print("Principal ($1K - $1M): ");
             principal = scanner.nextInt(); // int is enough (long too big - 64 bits)
 
-            if (principal < 1_000 || principal > 1_000_000) {
-                System.out.println("Enter a value greater than $1000 and less than or equal to $1,000,000");
+            if (principal > 1_000 && principal <= 1_000_000) {
+                break;
             }
-            else break;
+            System.out.println("Enter a value greater than $1000 and less than or equal to $1,000,000");
         }
 
-        float annualInterest;
         while (true) {
             System.out.print("Annual Interest Rate: ");
-            annualInterest = scanner.nextFloat(); // float is sufficient because interest rate has only a few decimal points
+            float annualInterest = scanner.nextFloat(); // float is sufficient because interest rate has only a few decimal points
 
-            if (annualInterest <= 0 || annualInterest > 30) {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30");
-                continue;
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthly = (annualInterest/PERCENT) / MONTHS_IN_YEAR;
+                break;
             }
-            else break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
         }
-        float monthly = (annualInterest/PERCENT) / MONTHS_IN_YEAR;
 
-        byte years; // byte is sufficient to store the number 30 or anything smaller
         while (true) {
             System.out.print("Period (Years): ");
-            years = scanner.nextByte();
+            // byte is sufficient to store the number 30 or anything smaller
+            byte years = scanner.nextByte();
 
-            if (years <= 0 || years > 30) {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            if (years > 0 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR; // int is sufficient (32 bits)
+                break;
             }
-            else break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
         }
 
-        int numberOfPayments = years * MONTHS_IN_YEAR; // int is sufficient (32 bits)
         double top = monthly * (Math.pow(1 + monthly, numberOfPayments));
         double bottom = (Math.pow(1 + monthly, numberOfPayments)) - 1;
         double total = principal * (top / bottom);
